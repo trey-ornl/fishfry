e50fdd914cfbb37b31dd040af40d4f034a8eb294
Trey White <twhite@pinoak-login1.head.cm.us.cray.com>
2022-03-22 13:26:28 -0500

    Tested build options -DPARIS_3PT and -DPARIS_5PT.

--- make.sh
+++ make.sh
@@ -5,6 +5,8 @@ module list
 set -x
 export CXX=hipcc
 export CXXFLAGS="-DO_HIP -g -O3 -std=c++11 --offload-arch=gfx90a -Wall -I${CRAY_MPICH_PREFIX}/include"
+#export CXXFLAGS="-DPARIS_5PT ${CXXFLAGS}"
+#export CXXFLAGS="-DPARIS_3PT ${CXXFLAGS}"
 export LD=CC
 export LDFLAGS="-DO_HIP -g -O3 -std=c++11 -Wall -L${ROCM_PATH}/lib"
 export LIBS='-lhipfft -lamdhip64'
